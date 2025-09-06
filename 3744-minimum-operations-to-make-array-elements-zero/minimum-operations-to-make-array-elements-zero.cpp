94588904f66d2e7c10c49227cc668ab097865ca2
Priyanshu Pal <151777536+Hustler002@users.noreply.github.com>
2025-09-06 17:33:53 +0530

    Time: 13 ms (74.64%) | Memory: 183.4 MB (64.49%) - LeetSync

--- 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
+++ 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
@@ -1,37 +1,66 @@
-#include <bits/stdc++.h>
-using namespace std;
+// class Solution {
+// public:
+//     long long minOperations(vector<vector<int>>& queries) {
+//         int ans = 0;
+//         for(auto &it : queries){
+//             priority_queue<int,vector<int>> pq;
+//             int l = it[0];
+//             int r = it[1];
+//             for(int i = l; i<= r; i++){
+//                 pq.push(i);
+//             }
+//             while(!pq.empty() && pq.size()>1){
+//                 int x = pq.top(); pq.pop();
+//                 int y = pq.top(); pq.pop();
+//                 x = x/4;
+//                 y = y/4;
+//                 ans++;
+//                 if(x!=0)pq.push(x);
+//                 if(y!=0)pq.push(y);
+//             }
+//             while(!pq.empty()){
+//                 int x = pq.top(); pq.pop();
+//                 x /=4;
+//                 ans++;
+//                 if(x!=0)pq.push(x);
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
+
+//OPTIMIZEDDDDDDDDDDDDD
 
 class Solution {
 public:
-    long long minOperations(vector<vector<int>>& queries) {
-        long long res = 0;
-
-        for (auto &it : queries) {
-            long long l = it[0];
-            long long r = it[1];
-
-            long long sum = 0;
-            long long low = 1;   // start of current block (4^{k-1})
-            int k = 1;
-            while (low <= r) {
-                long long high;
-                if (low > (LLONG_MAX / 4)) high = LLONG_MAX;
-                else high = low * 4 - 1;
-
-                long long a = max(low, l);
-                long long b = min(high, r);
-                if (a <= b) {
-                    sum += (b - a + 1) * 1LL * k;
+#define ll long long
+    ll minOperations(vector<vector<int>>& queries) {
+        ll ans = 0;
+        for(auto &it : queries){
+            ll l = it[0];
+            ll r = it[1];
+            ll step = 1;
+            ll L = 1;
+            ll R = 4*L-1;
+            ll op = 0;
+            while(L<=r){
+                int start = max(L,l);
+                int end = min(R,r);
+                if(start <= end){
+                    int num = end - start+1;
+                    op += step*num;
                 }
-
-                if (low > (LLONG_MAX / 4)) break;
-                low *= 4;
-                ++k;
+                step++;
+                L = 4*L;
+                R = 4*L-1;
             }
-
-            res += (sum + 1) / 2;
+             
+            ans += (op+1)/2;
         }
-
-        return res;
+        return ans;
     }
 };
