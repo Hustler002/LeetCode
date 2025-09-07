78f7cea0c864cbf31d4d32eafa5219b45459522a
Priyanshu Pal <151777536+Hustler002@users.noreply.github.com>
2025-09-07 13:37:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (53.26%) - LeetSync

--- 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
@@ -3,25 +3,11 @@ public:
     vector<int> sumZero(int n) {
         vector<int> ans;
         if(n==1) return {0};
-        if(n==2) return {-1,1};
-        if(n%2!=0){
-            ans.push_back(-1);
-            ans.push_back(0);
-            ans.push_back(1);
-            n=n-3;
-        }
-        else{
-            ans.push_back(-1);
-            ans.push_back(1);
-            n=n-2;
-        }
-        
-        int num = 2;
         for(int i = 1; i <= n/2; i++){
-            ans.push_back(num);
-            ans.push_back(-num);
-            num++;
+            ans.push_back(i);
+            ans.push_back(-i);
         }
+        if(n%2!=0) ans.push_back(0);
         return ans;
     }
 };
