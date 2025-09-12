5009255dd57b9903d639ee587d5a7a6a3952007e
Priyanshu Pal <151777536+Hustler002@users.noreply.github.com>
2025-09-13 01:10:34 +0530

    Time: 0 ms (100.00%) | Memory: 34.7 MB (26.67%) - LeetSync

---
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        
+        unordered_map<int,int> mpp;
+        for ( auto it : nums){
+            mpp[it]++;
+        }
+        int maxi = INT_MIN;
+        for ( auto it: mpp){
+            maxi = max(maxi,it.second);
+        }
+
+        vector<vector<int>> res;
+
+        while(!mpp.empty()){
+            vector<int> tmp;
+            for (auto it = mpp.begin(); it != mpp.end(); ) {
+                tmp.push_back(it->first);
+                // decrement count; if it reaches zero erase and update iterator safely
+                if (--(it->second) == 0) {
+                    it = mpp.erase(it); // erase returns next iterator (C++11+)
+                } else {
+                    ++it;
+                }
+            }
+            res.push_back(tmp);
+        }
+       // res.erase(res.begin());
+        return res;
+    }
+};
