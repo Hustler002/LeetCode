33fbb53a44112c159569125686636aa2f0b309fa
Priyanshu Pal <151777536+Hustler002@users.noreply.github.com>
2025-11-03 10:28:54 +0530

    Time: 755 ms (5.08%) | Memory: 99.8 MB (88.00%) - LeetSync

---
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minCost(string s, vector<int>& nt) {
+        int n = s.size();
+        int time = 0;
+        int total = accumulate(nt.begin(),nt.end(),0);
+        int sum = 0;
+        int i = 0;
+        while(i<n){
+            int maxi = nt[i];
+            char curr = s[i];
+            while(i < n && curr == s[i]){
+                maxi = max(maxi,nt[i]);
+                i++;
+            }
+            cout<<maxi<<endl;
+            sum += maxi;
+            
+        }
+        return total - sum;
+    }
+};
