a9332095d8689d714d542c2a41af529614dbb7f1
Priyanshu Pal <151777536+Hustler002@users.noreply.github.com>
2026-02-10 10:21:56 +0530

    Time: 1901 ms (61.01%) | Memory: 530.4 MB (41.51%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
@@ -5,7 +5,7 @@ public:
         int ans = 0;
         int n = nums.size();
         for(int left = 0; left < n; left++){
-            set<int> even, odd;
+            unordered_set<int> even, odd;
             for(int right = left; right < n; right++){
                 if(nums[right] % 2 == 0) even.insert(nums[right]);
                 else odd.insert(nums[right]);
