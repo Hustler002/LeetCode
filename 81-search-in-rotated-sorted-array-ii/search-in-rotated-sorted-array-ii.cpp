4c5a9268119cb7c33524d728a746cd968421ffe0
Priyanshu Pal <151777536+Hustler002@users.noreply.github.com>
2026-03-11 18:53:02 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (90.51%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -9,17 +9,22 @@ public:
 
             if(nums[mid] == target) return true;
 
-            if(nums[low] <= nums[mid]){
+            if(nums[low]==nums[mid] & nums[mid]==nums[high]){
+                low++;
+                high--;
+            }
+
+            else if(nums[low] <= nums[mid]){
                 if(nums[low] <= target && target <= nums[mid]){
                     high = mid-1;
                 }
-                else low = low+1;
+                else low = mid+1;
             }
             else{
                 if(nums[mid] <= target && target <= nums[high]){
                     low = mid+1;
                 }
-                else high = high-1;
+                else high = mid-1;
             }
         }
         return false;
