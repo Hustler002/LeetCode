08cac09507d6a3fb5b85f275ccb83ccb6a3a0437
Priyanshu Pal <151777536+Hustler002@users.noreply.github.com>
2026-03-16 23:06:52 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (64.31%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,15 +2,36 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int> mpp;
-        for(auto& it : nums) mpp[it]++;
-        
+        int limit = n/3;
+
         vector<int> ans;
-        for(auto& it : mpp){
-            if(it.second > ceil(n/3)){
-                ans.push_back(it.first);
+        int cnt1 = 0, cnt2 = 0;
+        int el1 = INT_MIN, el2 = INT_MIN;
+
+        for(int i = 0; i < n; i++){
+            if(cnt1 == 0 && nums[i] != el2){
+                cnt1 = 1;
+                el1 = nums[i];
             }
+            else if(cnt2 == 0 && nums[i] != el1){
+                cnt2 = 1;
+                el2 = nums[i];
+            }
+            else if(nums[i] == el1) cnt1++;
+            else if(nums[i] == el2) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
+            }
+
         }
+        cnt1 = 0, cnt2 = 0;
+        for(auto& it : nums){
+            if(it==el1) cnt1++;
+            if(it == el2) cnt2++;
+        }
+        if(cnt1 > limit) ans.push_back(el1);
+        if(cnt2 > limit) ans.push_back(el2);
         return ans;
     }
 };
