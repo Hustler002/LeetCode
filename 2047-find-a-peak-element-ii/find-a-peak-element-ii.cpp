69e0d5db1b79b16b591b11fdac0e7e123bccee7b
Priyanshu Pal <priyanshupalpp2203@gmail.com>
2026-03-22 23:47:03 +0530

    Time: 0 ms (100.00%) | Memory: 50.3 MB (92.77%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -1,14 +1,35 @@
 class Solution {
 public:
+    pair<int,int> findMax(vector<vector<int>>& mat , int mid, int n, int m){
+        pair<int,int> index = {-1,-1};
+        int maxi = -1;
+        for(int i = 0; i < m; i++){
+            if(mat[i][mid] > maxi){
+                maxi = mat[i][mid];
+                index = {i,mid};
+            }
+        }
+        return index;
+    }
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if((i-1 >= 0 ? mat[i][j] > mat[i-1][j] : 1) && (i+1 < m ? mat[i][j] > mat[i+1][j] : 1) && (j-1 >= 0 ? mat[i][j] > mat[i][j-1]: 1) && (j+1 < n ? mat[i][j] > mat[i][j+1] : 1)) return {i,j};
-            }
+        int low = 0, high = n-1;
+
+        while(low <= high){
+            int mid = low + (high-low)/2;
+
+            pair<int,int> maxi = findMax(mat, mid, n, m);
+            int i = maxi.first;
+            cout<<mat[i][mid];
+            if((mid-1 >=0 ? mat[i][mid-1] < mat[i][mid]: 1) && (mid+1 < n ? mat[i][mid+1] < mat[i][mid] : 1))
+                return {i,mid};
+            else if((mid-1 >=0 ? mat[i][mid-1] < mat[i][mid]: 1))
+                low = mid+1;
+            else if(mid+1 < n ? mat[i][mid+1] < mat[i][mid] : 1) 
+                high = mid-1;
         }
-       return {-1,-1};
+        return {-1,-1};
     }
 };
