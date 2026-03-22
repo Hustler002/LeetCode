12693e7680efdf01b09a29454bcc1aeac1982dd4
Priyanshu Pal <priyanshupalpp2203@gmail.com>
2026-03-22 14:12:27 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (41.55%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -1,51 +1,28 @@
 class Solution {
 public:
-    
-    bool check(vector<vector<int>>& mat, vector<vector<int>>& target){
-        int n = mat.size();
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(mat[i][j] != target[i][j]) return false;
-            }
-        }
-        return true;
-    }
-
     void rotate(vector<vector<int>>& mat){
         int n = mat.size();
         //transpose
         for(int i = 0; i < n; i++){
             for(int j = i+1; j < n; j++){
-                int temp = mat[i][j];
-                mat[i][j] = mat[j][i];
-                mat[j][i] = temp;
+                swap(mat[i][j], mat[j][i]);
             }
         }
 
         // reverse the columns
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n/2; j++){
-                int temp = mat[i][j];
-                mat[i][j] = mat[i][n-j-1];
-                mat[i][n-j-1] = temp;
-            }
+            reverse(mat[i].begin(), mat[i].end());
         }
         return;
-
     }
 
     bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
         int n = mat.size();
-        if(check(mat,target)) return true;
-
-        rotate(mat);
-        if(check(mat,target)) return true;
-
-        rotate(mat);
-        if(check(mat,target)) return true;
-
-        rotate(mat);
-        if(check(mat,target)) return true;
+        
+        for(int i = 0; i < 4; i++){
+            if(mat == target) return true;
+            rotate(mat);
+        }
 
         return false;
     }
