791814ccdc652c466657f95f5b9b9c667855c119
Priyanshu Pal <priyanshupalpp2203@gmail.com>
2026-03-24 20:04:35 +0530

    Time: 39 ms (51.66%) | Memory: 132 MB (80.07%) - LeetSync

--- 3031-construct-product-matrix/construct-product-matrix.cpp
+++ 3031-construct-product-matrix/construct-product-matrix.cpp
@@ -1,54 +1,29 @@
 class Solution {
 public:
-    int MOD = 12345;
     vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-
-        vector<int> pre(n*m, 1), suff(n*m, 1);
-
-        int k = 0;
-        pre[k] = grid[0][0] % MOD;
-        k++;
-        for(int i = 0; i< n; i++){
-            for(int j = 0; j < m; j++){
-                if(i==0 && j==0) continue;
-                
-                pre[k] = (1LL * pre[k-1] * grid[i][j]) % MOD; 
-                k++;
+        vector<vector<int>> p(n, vector<int>(m, 1));
+        long long MOD = 12345;
+        
+        // Pass 1: Compute and store prefix products directly into 'p'
+        long long prefix = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                p[i][j] = prefix; // Store the product of all elements before (i, j)
+                prefix = (prefix * grid[i][j]) % MOD; // Update running prefix
             }
         }
         
-        k = (n*m)-1;
-        suff[k] = grid[n-1][m-1] % MOD;
-        k--;
-        for(int i = n-1; i >= 0; i--){
-            for(int j = m-1; j >= 0; j--){
-                if(i==n-1 && j==m-1) continue;
-                suff[k] = (1LL * suff[k+1] * grid[i][j]) % MOD;
-                k--; 
+        // Pass 2: Compute suffix products on the fly and multiply with 'p'
+        long long suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                p[i][j] = (p[i][j] * suffix) % MOD; // Multiply prefix by suffix
+                suffix = (suffix * grid[i][j]) % MOD; // Update running suffix
             }
         }
         
-        vector<int> ans(m*n, 1);
-        ans[0] = suff[1];
-        ans[(n*m-1)] = pre[(n*m-2)];
-
-        for(int i = 1; i < n*m-1; i++){
-            ans[i] = (1LL * pre[i-1] * suff[i+1]) % MOD;
-        }
-
-        vector<vector<int>> pro;
-        k = 0;
-        for(int i = 0; i < n; i++){
-            vector<int> temp;
-            for(int j = 0; j < m; j++){
-                temp.push_back(ans[k]);
-                k++;
-            }
-            pro.push_back(temp);
-        }
-
-        return pro;
+        return p;
     }
 };
