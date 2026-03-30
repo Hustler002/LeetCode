7e4ee4fa8b2a749552363b05893eb5d46dcda55e
Priyanshu Pal <priyanshupalpp2203@gmail.com>
2026-03-30 19:32:23 +0530

    Time: 4 ms (83.33%) | Memory: 19.8 MB (62.64%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -1,25 +1,54 @@
+// class Solution {
+// public:
+//     bool checkStrings(string s1, string s2) {
+//         int n = s1.size();
+//         unordered_map<char,int> odd1, even1;
+
+//         for(int i = 0; i < n; i++){
+//             if(i%2==0)
+//                 even1[s1[i]]++;
+//             else 
+//                 odd1[s1[i]]++;
+//         }
+            
+
+//         unordered_map<char,int> odd2, even2;
+
+//         for(int i = 0; i < n; i++){
+//             if(i%2==0)
+//                 even2[s2[i]]++;
+//             else 
+//                 odd2[s2[i]]++;
+//         }
+//         return (odd1==odd2 && even1 == even2);
+//     }
+// };
+
+
+// Approach 2
+
 class Solution {
 public:
     bool checkStrings(string s1, string s2) {
         int n = s1.size();
-        unordered_map<char,int> odd1, even1;
+        
+        int even[26] = {0};
+        int odd[26] = {0};
 
         for(int i = 0; i < n; i++){
-            if(i%2==0)
-                even1[s1[i]]++;
-            else 
-                odd1[s1[i]]++;
+            if(i % 2 == 0){
+                even[s1[i] - 'a']++;
+                even[s2[i] - 'a']--;
+            }
+            else{
+                odd[s1[i] - 'a']++;
+                odd[s2[i] - 'a']--;
+            }
         }
-            
 
-        unordered_map<char,int> odd2, even2;
-
-        for(int i = 0; i < n; i++){
-            if(i%2==0)
-                even2[s2[i]]++;
-            else 
-                odd2[s2[i]]++;
+        for(int i = 0; i < 26; i++){
+            if(even[i] != 0 || odd[i] != 0) return false;
         }
-        return (odd1==odd2 && even1 == even2);
+        return true;
     }
 };
