f2dae0df7c526726796719b25c7fdb39cbc1cb53
Priyanshu Pal <priyanshupalpp2203@gmail.com>
2026-04-22 15:32:05 +0530

    Time: 19 ms (29.41%) | Memory: 25 MB (24.60%) - LeetSync

---
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int diff(string q, string d){
+        int n = q.size();
+        int cnt  = 0;
+        for(int i = 0; i < n; i++){
+            if(q[i] != d[i]) cnt++;
+            if(cnt > 2) return false;
+        }
+        return true;
+    }
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        vector<string> res;
+        for(auto& q : queries){
+            for(auto& d : dictionary){
+                if(diff(q,d)){
+                    res.push_back(q);
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
