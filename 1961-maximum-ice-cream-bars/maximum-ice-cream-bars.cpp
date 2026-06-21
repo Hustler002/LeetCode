96118633a4f2bb54744ac110f763d6d4915eb40b
Priyanshu Pal <priyanshupalpp2203@gmail.com>
2026-06-22 00:14:44 +0530

    Time: 13 ms (79.51%) | Memory: 84.2 MB (25.28%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
@@ -1,7 +1,24 @@
 class Solution {
 public:
+    void counting_sort(vector<int>& nums){
+        int maxi = *max_element(nums.begin(), nums.end());
+
+        vector<int> freq(maxi+1,0);
+
+        for(int i = 0; i < nums.size(); i++){
+            freq[nums[i]]++;
+        }
+        int idx = 0;
+        for(int i = 0; i < freq.size(); i++){
+            while(freq[i]--) {
+                nums[idx] = i;
+                idx++;
+            }
+        }
+    }
     int maxIceCream(vector<int>& costs, int coins) {
-        sort(costs.begin(), costs.end());
+        
+        counting_sort(costs);
         int i = 0;
         int cnt = 0;
         while(i < costs.size() && coins >= costs[i]){
