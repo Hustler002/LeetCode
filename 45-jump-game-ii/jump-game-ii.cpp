01bbfbb33b42b156eeb70adfc74f0fc33fd0eb37
Priyanshu Pal <priyanshupalpp2203@gmail.com>
2026-08-14 13:06:14 +0530

    Time: 43 ms (23.10%) | Memory: 21.8 MB (19.95%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
-    int fun(vector<int>& dp, int i, vector<int>& nums){
-        int n = nums.size();
-        if(i>=n-1) return 0;
+    // int fun(vector<int>& dp, int i, vector<int>& nums){
+    //     int n = nums.size();
+    //     if(i>=n-1) return 0;
 
-        if(dp[i] != -1) return dp[i];
+    //     if(dp[i] != -1) return dp[i];
 
-        int mini = 1e8;
-        for(int j = i+1; j<= nums[i] + i; j++){
-            mini = min (mini, 1 + fun(dp, j, nums));
-        }
+    //     int mini = 1e8;
+    //     for(int j = i+1; j<= nums[i] + i; j++){
+    //         mini = min (mini, 1 + fun(dp, j, nums));
+    //     }
 
-        return dp[i] = mini;
-    }
+    //     return dp[i] = mini;
+    // }
     int jump(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> dp(n,-1);
+        vector<int> dp(n,0);
+        dp[n-1] = 0;
 
-        return fun(dp, 0, nums);
+        for(int i = n-2; i >= 0 ; i--){
+            int mini = 1e8;
+            for(int j = i+1; j<= nums[i] + i; j++){
+                if( j < n) mini = min (mini, 1 + dp[j]);
+            }
+            dp[i] = mini;
+        }
 
+        return dp[0];
     }
 };
